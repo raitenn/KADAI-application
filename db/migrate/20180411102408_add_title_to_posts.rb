63e1a01d68c180b3b07f84895f6ece810b2a9cfe
raitenn <rakurusurann@gmail.com>
2018-04-14 08:55:39 +0000

    修正しました

--- db/migrate/20180411102408_add_title_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddTitleToPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :posts, :title, :string
-    add_column :posts, :body, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411102408) do
+ActiveRecord::Schema.define(version: 20180411082905) do
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
